d94e908fa20f88e92d9fa7e0cbd31c1f8cdc85dc
hyungjae.lee <dlgudwo11@gmail.com>
2025-04-23 13:31:17 +0900

    kafka-outbox-pattern init commit

---
+++ src/main/java/com/hj/kafkaoutboxpattern/KafkaOutboxPatternApplication.java
@@ -0,0 +1,13 @@
+package com.hj.kafkaoutboxpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOutboxPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOutboxPatternApplication.class, args);
+    }
+
+}
